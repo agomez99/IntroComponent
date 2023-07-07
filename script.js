839629907edfa72600fde64a7297837fcc4eb12d
Austine Gomez <austine.gomez@yahoo.com>
2023-07-06 21:56:22 -0500

    testtin javascriupyt

--- script.js
+++ script.js
@@ -2,56 +2,49 @@ function handleFormSubmission(event) {
   event.preventDefault();
 
   const inputs = ['first-name', 'last-name', 'email', 'password'];
-  const errors = ['icon1', 'icon2','icon3'];
   const errorMessages = ['First name cannot be empty.', 'Last name cannot be empty.', 'Email cannot be empty.', 'Password cannot be empty.'];
+  const errorIcons = ['.error-icon', '.error-icon2', '.error-icon3'];
 
   for (let i = 0; i < inputs.length; i++) {
     const value = document.getElementById(inputs[i]).value;
     const errorMessage = document.getElementById('error-' + inputs[i]);
-    const errorsIcon = document.getElementById('error-' +inputs[i])
-    var errorIcon = document.querySelector(".error-icon");
-
+    const errorIcon = document.querySelector(errorIcons[i]);
 
     if (value === '') {
       errorMessage.textContent = errorMessages[i];
       document.getElementById(inputs[i]).style.borderColor = 'red';
-
       errorIcon.style.visibility = "visible";
-      errorsIcon.style.visibility = "visible";
-
       return;
-      
     }
     errorMessage.textContent = '';
-    errorIcon.style.visibility = "hidden";
-
-        
-      if (value === "") {
-        errorIcon.style.visibility = "visible";
-      } else {
-        errorIcon.style.visibility = "hidden";
-      }
-      }
-      alert('Form submitted successfully!');
-      document.getElementById('myForm').reset();
-
-    }
-
-const inputField = document.getElementById('first-name');
-
-const clearErrorMessage = () => {
-  const showError = (inputId, errorId) => {
-    const inputField = document.getElementById(inputId);
-    const errorMessage = document.getElementById(errorId);
-   const errorIcon = document.querySelector(".error-icon");
-
-    inputField.style.borderColor = '';
-    errorMessage.style.display = inputField.value ? 'none' : '';
+    document.getElementById(inputs[i]).style.borderColor = '';
     errorIcon.style.visibility = "hidden";
 
   }
 
+
+  alert('Form submitted successfully!');
+
+  document.getElementById('myForm').reset();
+  clearErrorMessage();
+}
+
+
+
+const clearErrorMessage = () => {
+  const showError = (inputId, errorId, errorIcon) => {
+    const inputField = document.getElementById(inputId);
+    const errorMessage = document.getElementById(errorId);
+    const errorIcons = document.getElementById(errorIcon);
+
+    inputField.style.borderColor = '';
+    errorMessage.style.display = inputField.value ? 'none' : '';
+    errorIcons.style.visibility = 'hidden';
+
   showError('first-name', 'error-first-name');
   showError('last-name', 'error-last-name');
   showError('password', 'error-password');
+  showError('email', 'error-email');l
 };
+
+}
