44bf79a4c16236bc60a11a45947ca5e8ab6ebf2e
Austine Gomez <austine.gomez@yahoo.com>
2023-07-07 23:55:57 -0500

    working scripts

--- script.js
+++ script.js
@@ -1,50 +1,81 @@
-function handleFormSubmission(event) {
-  event.preventDefault();
+window.addEventListener('DOMContentLoaded', function() {
+  const formInputs = {
+    firstName: document.getElementById('firstName'),
+    lastName: document.getElementById('lastName'),
+    email: document.getElementById('email'),
+    password: document.getElementById('password')
+  };
 
-  const inputs = ['first-name', 'last-name', 'email', 'password'];
-  const errorMessages = ['First name cannot be empty.', 'Last name cannot be empty.', 'Email cannot be empty.', 'Password cannot be empty.'];
-  const errorIcons = ['.error-icon', '.error-icon2', '.error-icon3'];
+  Object.values(formInputs).forEach((input) => {
+    input.addEventListener('input', clearError);
+  });
 
-  for (let i = 0; i < inputs.length; i++) {
-    const value = document.getElementById(inputs[i]).value;
-    const errorMessage = document.getElementById('error-' + inputs[i]);
-    const errorIcon = document.querySelector(errorIcons[i]);
+  function clearError(event) {
+    const inputElement = event.target;
+    const errorElement = document.getElementById(inputElement.id + 'Error');
+    const iconElement = document.getElementById(inputElement.id + 'Icon');
+    const borderElement = document.getElementById(inputElement.id);
 
-    if (value === '') {
-      errorMessage.textContent = errorMessages[i];
-      document.getElementById(inputs[i]).style.borderColor = 'red';
-      errorIcon.style.visibility = "visible";
-      return;
-    }
-    errorMessage.textContent = '';
-    document.getElementById(inputs[i]).style.borderColor = '';
-    errorIcon.style.visibility = "hidden";
+    errorElement.textContent = '';
+    iconElement.style.display = 'none';
+    borderElement.style.border = '1px solid #ccc';
+    inputElement.classList.remove('error-field');
+  }
+});
 
+function validateForm() {
+  const formInputs = {
+    firstName: document.getElementById('firstName'),
+    lastName: document.getElementById('lastName'),
+    email: document.getElementById('email'),
+    password: document.getElementById('password')
+  };
+
+  const errorMessages = ['firstNameError', 'lastNameError', 'emailError', 'passwordError'];
+  const icons = ['firstNameIcon', 'lastNameIcon', 'emailIcon', 'passwordIcon'];
+
+  errorMessages.forEach((error) => {
+    document.getElementById(error).textContent = '';
+  });
+
+  icons.forEach((icon) => {
+    document.getElementById(icon).style.display = 'none';
+  });
+
+  Object.values(formInputs).forEach((input) => {
+    input.classList.remove('error-field');
+  });
+
+  let isValid = true;
+
+  if (formInputs.firstName.value === '') {
+    setError(formInputs.firstName, 'First name cannot be empty');
+    isValid = false;
   }
 
+  if (formInputs.lastName.value === '') {
+    setError(formInputs.lastName, 'Last name cannot be empty');
+    isValid = false;
+  }
 
-  alert('Form submitted successfully!');
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (formInputs.email.value === '' || !formInputs.email.value.match(mailformat)) {
+    setError(formInputs.email, 'Looks like this is not an email');
+    isValid = false;
+  } 
 
-  document.getElementById('myForm').reset();
-  clearErrorMessage();
+  if (formInputs.password.value === '' || formInputs.password.value.length < 8) {
+    setError(formInputs.password, 'Password must be at least 8 characters long');
+    isValid = false;
+  }
+
+  return isValid;
 }
 
-
-
-const clearErrorMessage = () => {
-  const showError = (inputId, errorId, errorIcon) => {
-    const inputField = document.getElementById(inputId);
-    const errorMessage = document.getElementById(errorId);
-    const errorIcons = document.getElementById(errorIcon);
-
-    inputField.style.borderColor = '';
-    errorMessage.style.display = inputField.value ? 'none' : '';
-    errorIcons.style.visibility = 'hidden';
-
-  showError('first-name', 'error-first-name');
-  showError('last-name', 'error-last-name');
-  showError('password', 'error-password');
-  showError('email', 'error-email');l
-};
-
+function setError(input, errorMessage) {
+  input.style.border = '1px solid red';
+  document.getElementById(input.id + 'Error').textContent = errorMessage;
+  document.getElementById(input.id + 'Icon').style.display = 'block';
+  input.classList.add('error-field');
 }
+
